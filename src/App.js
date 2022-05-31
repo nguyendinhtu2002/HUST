95495e959b68051c6cd3885475a9ebeace0ef5ef
nguyendinhtu2002 <nguyendinhtu11022002@gmail.com>
2022-06-01 00:52:50 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 import './App.css';
-import { Header } from './pages/Header';
-import { HeaderBt } from './pages/HeaderBt';
-import {Slider} from './pages/Carousel';
-import { Content } from './pages/Content';
-import { News } from './pages/News';
-import { DaoTao } from './pages/Daotao';
-import { CuuSv } from './pages/CuuSv';
-import { Banner } from './pages/Banner';
-import { Footer } from './pages/Footer';
-import { Talk } from './pages/Talk';
-import { Contact } from './pages/Contact';
-import { Video } from './pages/Video';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Index';
+import Tintuc from './pages/Tintuc';
+
 function App() {
   return (
-  
+
     <div className="App">
-      <Header />
-      <HeaderBt/>
-      <Slider/>
-      <Content/>
-      <News/>
-      <DaoTao/>
-      <Banner/>
-      <CuuSv/>
-      <Talk />
-      <Contact/>
-      <Video />
-      <Footer/>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/tintuc" element={<Tintuc />} /> 
+        </Routes>
+      </BrowserRouter>
+
 
     </div>
-    
+
   );
 }
 
--- src/pages/HeaderBt.js
+++ src/pages/HeaderBt.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { FaAngleDown } from 'react-icons/fa'
+import { Link } from "react-router-dom";
 
 export function HeaderBt() {
   return (
-    <nav className="navbar navbar-expand-lg shadow-lg   bg-zinc-800 relative flex items-center w-full justify-between border-t-4 border-red-600 pl-32">
+    <nav className="navbar navbar-expand-lg shadow-lg md:flex  bg-zinc-800 z-50 sticky top-0 flex items-center w-full justify-between border-t-4 border-red-600 md:pl-80">
       <div className="px-6 max-w-screen-lg">
         <button className="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-white hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContentX" aria-controls="navbarSupportedContentX" aria-expanded="false" aria-label="Toggle navigation">
           <svg aria-hidden="true" focusable="false" data-prefix="fas" className="w-5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
@@ -20,7 +20,7 @@ export function HeaderBt() {
                   <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
                 </svg>
               </a>
-              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute left-44 top-full " aria-labelledby="dropdownMenuButtonX">
+              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute 4 top-full " aria-labelledby="dropdownMenuButtonX">
                 <div className="px-6 lg:px-8 py-5">
                   <div className="grid md:grid-cols-2 lg:grid-cols-1 ">
                     <div className="bg-slate-800 text-white ">
@@ -45,7 +45,7 @@ export function HeaderBt() {
                   <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
                 </svg>
               </a>
-              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute left-56 top-full " aria-labelledby="dropdownMenuButtonX">
+              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute  top-full " aria-labelledby="dropdownMenuButtonX">
                 <div className="px-6 lg:px-8 py-5">
                   <div className="grid md:grid-cols-2 lg:grid-cols-2 ">
                     <div className="bg-slate-800 text-white">
@@ -74,7 +74,7 @@ export function HeaderBt() {
                   <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
                 </svg>
               </a>
-              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute left-4 top-full " aria-labelledby="dropdownMenuButtonX">
+              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute  top-full " aria-labelledby="dropdownMenuButtonX">
                 <div className="px-6 lg:px-8 py-5">
                   <div className="grid md:grid-cols-2 lg:grid-cols-1 ">
                     <div className="bg-slate-800 text-white ">
@@ -98,7 +98,7 @@ export function HeaderBt() {
                   <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
                 </svg>
               </a>
-              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute left-4 top-full " aria-labelledby="dropdownMenuButtonX">
+              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute  top-full " aria-labelledby="dropdownMenuButtonX">
                 <div className="px-6 lg:px-8 py-5">
                   <div className="grid md:grid-cols-2 lg:grid-cols-1 ">
                     <div className="bg-slate-800 text-white ">
@@ -122,7 +122,7 @@ export function HeaderBt() {
                   <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
                 </svg>
               </a>
-              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute left-4 top-full " aria-labelledby="dropdownMenuButtonX">
+              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute  top-full " aria-labelledby="dropdownMenuButtonX">
                 <div className="px-6 lg:px-8 py-5">
                   <div className="grid md:grid-cols-2 lg:grid-cols-1 ">
                     <div className="bg-slate-800 text-white ">
@@ -146,7 +146,7 @@ export function HeaderBt() {
                   <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
                 </svg>
               </a>
-              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute left-4 top-full " aria-labelledby="dropdownMenuButtonX">
+              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute  top-full " aria-labelledby="dropdownMenuButtonX">
                 <div className="px-6 lg:px-8 py-5">
                   <div className="grid md:grid-cols-2 lg:grid-cols-1 ">
                     <div className="bg-slate-800 text-white ">
@@ -170,7 +170,7 @@ export function HeaderBt() {
                   <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
                 </svg>
               </a>
-              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute left-4 top-full " aria-labelledby="dropdownMenuButtonX">
+              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute  top-full " aria-labelledby="dropdownMenuButtonX">
                 <div className="px-6 lg:px-8 py-5">
                   <div className="grid md:grid-cols-2 lg:grid-cols-1 ">
                     <div className="bg-slate-800 text-white ">
@@ -194,7 +194,7 @@ export function HeaderBt() {
                   <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
                 </svg>
               </a>
-              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute left-4 top-full " aria-labelledby="dropdownMenuButtonX">
+              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute  top-full " aria-labelledby="dropdownMenuButtonX">
                 <div className="px-6 lg:px-8 py-5">
                   <div className="grid md:grid-cols-2 lg:grid-cols-1 ">
                     <div className="bg-slate-800 text-white ">
@@ -212,23 +212,29 @@ export function HeaderBt() {
               </div>
             </li>
             <li className="nav-item dropdown static border-r border-slate-700">
-              <a className="nav-link block pr-2 lg:px-2 py-2 text-white hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out dropdown-toggle flex items-center whitespace-nowrap" href="#" data-mdb-ripple="true" data-mdb-ripple-color="light" type="button" id="dropdownMenuButtonX" data-bs-toggle="dropdown"
+              <a className="nav-link block pr-2 lg:px-2 py-2 text-white hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out dropdown-toggle flex items-center whitespace-nowrap" to="TinTuc" data-mdb-ripple="true" data-mdb-ripple-color="light" type="button" id="dropdownMenuButtonX" data-bs-toggle="dropdown"
                 aria-expanded="false">Tin tức-Sự kiện
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="caret-down" className="w-2 ml-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                   <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
                 </svg>
               </a>
-              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute left-4 top-full " aria-labelledby="dropdownMenuButtonX">
-                <div className="px-6 lg:px-8 py-5">
-                  <div className="grid md:grid-cols-2 lg:grid-cols-1 ">
+              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute  top-full " aria-labelledby="dropdownMenuButtonX">
+                <div className="py-5">
+                  <div className="flex gap-5 ">
+                    <div className="bg-slate-800 text-white">
+                      <Link to="Tintuc" aria-current="true" className="block py-2 px-8 border-b border-slate-700 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out bg-red-600">Tin tức</Link>
+                      <a href="#!" aria-current="true" className="block py-2 px-8 border-b border-slate-700 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">Thông báo</a>
+                      <a href="#!" aria-current="true" className="block py-2 px-8 border-b border-slate-700 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">Tin bài</a>
+                      <a href="#!" aria-current="true" className="block py-2 px-8  hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">SoICT Talks</a>
+
+
+                    </div>
+                    <div className="w-px h-40 bg-white"></div>
                     <div className="bg-slate-800 text-white ">
-                      <a href="#!" aria-current="true" className="block py-2 border-b border-slate-700 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">Thông tin chung</a>
-                      <a href="#!" aria-current="true" className="block py-2 border-b border-slate-700 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">Triết lý giáo dục</a>
-                      <a href="#!" aria-current="true" className="block py-2 border-b border-slate-700 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">Cơ cấu tổ chức</a>
-                      <a href="#!" aria-current="true" className="block py-2 border-b border-slate-700 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">Đảng ủy - Hội đồng Trường</a>
-                      <a href="#!" aria-current="true" className="block py-2  border-b w-full border-slate-700 hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">Các Tổ chức Đoàn thể </a>
-                      <a href="#!" aria-current="true" className="block py-2  border-b w-full border-slate-700 hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">Văn phòng Trưởng</a>
-                      <a href="#!" aria-current="true" className="block py-2  w-full border-slate-700 hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">Danh sách Cán bộ</a>
+                      <a href="#!" aria-current="true" className="block py-2 px-8 border-b border-slate-700 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out bg-red-600">Sự kiện</a>
+                      <a href="#!" aria-current="true" className="block py-2 px-8 border-b border-slate-700 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">Sự kiện sắp diễn ra</a>
+                      <a href="#!" aria-current="true" className="block py-2 px-8  border-slate-700 w-full hover:bg-gray-50 hover:text-gray-700 transition duration-150 ease-in-out">Sự kiện đang diễn ra</a>
+                      
                     </div>
 
                   </div>
@@ -242,7 +248,7 @@ export function HeaderBt() {
                   <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
                 </svg>
               </a>
-              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute left-4 top-full " aria-labelledby="dropdownMenuButtonX">
+              <div className="dropdown-menu w-70 mt-0 hidden shadow-lg bg-slate-800 absolute  top-full " aria-labelledby="dropdownMenuButtonX">
                 <div className="px-6 lg:px-8 py-5">
                   <div className="grid md:grid-cols-2 lg:grid-cols-1 ">
                     <div className="bg-slate-800 text-white ">
